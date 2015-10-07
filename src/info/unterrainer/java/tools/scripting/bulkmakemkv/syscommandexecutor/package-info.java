294d3fcf3c52a9c7ac8040af1c5ad14facb3e972
psilo <psilo@unterrainer.info>
2015-10-07 22:13:26 +0200

    Corrected typos.

--- src/info/unterrainer/java/tools/scripting/bulkmakemkv/syscommandexecutor/package-info.java
+++ src/info/unterrainer/java/tools/scripting/bulkmakemkv/syscommandexecutor/package-info.java
@@ -1,5 +1,5 @@
 /**
- * This package contains all necessary classes for the implementaion of the {@link SysCommandExecutor}.
+ * This package contains all necessary classes for the implementation of the {@link SysCommandExecutor}.
  */
 @javax.annotation.ParametersAreNonnullByDefault
 package info.unterrainer.java.tools.scripting.bulkmakemkv.syscommandexecutor;
