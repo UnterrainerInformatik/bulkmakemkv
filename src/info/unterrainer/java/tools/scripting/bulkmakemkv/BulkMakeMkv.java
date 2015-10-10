348e3c33f75d191ebf40603b0c8d1326634e3668
psilo <psilo@unterrainer.info>
2015-10-10 20:13:55 +0200

    Removed mac-switch for path-escaping after realizing that mac-bash == unix-bash. Corrected mishap in parameter quoting in makemkvcon call.

--- src/info/unterrainer/java/tools/scripting/bulkmakemkv/BulkMakeMkv.java
+++ src/info/unterrainer/java/tools/scripting/bulkmakemkv/BulkMakeMkv.java
@@ -64,8 +64,6 @@ public class BulkMakeMkv {
 	private static String makeMkvCommand;
 	private static String makeMkvTempFileExtension;
 
-	private static String os;
-
 	private static List<Path> cache;
 
 	public static void main(String[] args) {
@@ -84,11 +82,6 @@ public class BulkMakeMkv {
 		if (!mode.equals("convert") && !mode.equals("scan")) {
 			Utils.sysout("You have to specify a valid mode!");
 			System.exit(1);
-		}
-
-		os = config.getString("os");
-		if (os == null || os.isEmpty() || !os.equals("mac")) {
-			os = "windows";
 		}
 
 		// Get parameter isoDirs.
@@ -346,14 +339,7 @@ public class BulkMakeMkv {
 	}
 
 	private static boolean doConvert(FileName file, String isoDir) {
-		String command = "\"" + makeMkvCommand + "\" mkv iso:\"" + isoDir + file.getName() + "." + file.getExtension() + "\" all \"" + tempDir + "\"";
-		if (os.equals("mac")) {
-			command = makeMkvCommand.replace(" ", "\\ ")
-					+ " mkv iso:"
-					+ (isoDir + file.getName() + "." + file.getExtension()).replace(" ", "\\ ")
-					+ " all "
-					+ tempDir.replace(" ", "\\ ");
-		}
+		String command = "\"" + makeMkvCommand + "\" mkv \"iso:" + isoDir + file.getName() + "." + file.getExtension() + "\" all \"" + tempDir + "\"";
 		Utils.sysout(command);
 		return doCommand(command);
 	}
